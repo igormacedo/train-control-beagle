eb8e82746697b2045e8dd48464121c7a11cb7d6f
Igor Macedo <igormacedo94@yahoo.com.br>
2017-11-05 22:01:45 -0300

    initial commit, preparing project

---
+++ include/gpio.h
@@ -0,0 +1,27 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+
+/****************************************************************
+* Constants
+****************************************************************/
+
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+#define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
+#define MAX_BUF 64
+
+
+/****************************************************************
+* Functions
+****************************************************************/
+
+int gpio_export(unsigned int gpio);
+int gpio_unexport(unsigned int gpio);
+int gpio_set_dir(unsigned int gpio, unsigned int out_flag);
+int gpio_set_value(unsigned int gpio, unsigned int value);
+int gpio_get_value(unsigned int gpio, unsigned int *value);
+int gpio_set_edge(unsigned int gpio, char *edge);
+int gpio_fd_open(unsigned int gpio);
+int gpio_fd_close(int fd);
+
+#endif
---
+++ src/lcd.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "lcd.h"
+
+int lcd_open(char *devpath) {
+    int lcd_fd = open(devpath, O_WRONLY);
+    if (lcd_fd < 0) {
+        perror("lcd_open: ");
+        exit(1);
+    }
+    return lcd_fd;
+}
+
+void lcd_message(int lcd_fd, char *fmt, ...) {
+    va_list vl;
+    va_start(vl, fmt);
+    lcd_len = vsnprintf(lcd_buf, sizeof(lcd_buf), 
+                       fmt, vl);
+    va_end(vl);
+    write(lcd_fd, lcd_buf, lcd_len);
+}
+
+void lcd_clear_screen(int lcd_fd) {
+    lcd_message(lcd_fd, "\033[2J");
+}
+
+void lcd_go_home(int lcd_fd) {
+    lcd_message(lcd_fd, "\033[H");
+}
+
+void lcd_clear_and_home(int lcd_fd) {
+    lcd_clear_screen(lcd_fd);
+    lcd_go_home(lcd_fd);
+}
